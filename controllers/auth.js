e6993e85f5788ef08d6128a0d7095f9726fbf277
Asdrúbal Mejía López <asdrumejia19@gmail.com>
2023-02-14 11:03:55 -0500

    Fin seccion 4 - version 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
-const { response } = require('express');
+const { response, json } = require('express');
 const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async(req, res = response) => {
@@ -51,6 +52,60 @@ const login = async(req, res = response) => {
 };
 
 
+const googleSingIn = async(req, res = response) => {
+     
+    const { id_token } = req.body;
+
+    try {
+
+        const {nombre, img, correo} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo})
+        
+        //Si el usuario existe o no
+        if(!usuario){
+            //Tengo que crearlo
+            const data = {
+                nombre,
+                img,
+                correo,
+                password: ':P',
+                role: 'ADMIN_ROLE',
+                google: true,
+            }
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        //Si el usuario en DB
+        if(!usuario.estado){
+            res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        //Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+           usuario, 
+           token
+        })
+        
+    } catch (error) {
+
+        res.status(400).json({
+            msg: 'Token de Google no es valido'
+        })
+    }
+};
+
+
 module.exports = {
-   login
+   login,
+   googleSingIn
 }
+
+
+
